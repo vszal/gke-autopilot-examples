101101858e9e75c2745e367acca39ecc3e754bbb
vsz <victor.szalvay@gmail.com>
2023-04-04 23:45:31 +0000

    better handling of PROJECT_ID variable

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,11 +1,12 @@
-#if [[ -z "${PROJECT_ID}" ]]; then
-#  echo "The value of PROJECT_ID is not set. Be sure to run export PROJECT_ID=YOUR-PROJECT first"
-# return
-#fi
 # if running outside of Cloud Shell tutorial, uncomment - sets the current project for gcloud
 #gcloud config set project $PROJECT_ID
 # Set $PROJECT_ID environment variable to current project value
 export PROJECT_ID=$(gcloud config get-value project)
+# exit if PROJECT_ID is not set
+if [[ -z "${PROJECT_ID}" ]]; then
+  echo "The value of PROJECT_ID is not set. Be sure to run export PROJECT_ID=YOUR-PROJECT first"
+  return
+fi
 # Enables the GKE API
 gcloud services enable container.googleapis.com 
 # Create demo cluster
--- bootstrap/teardown.sh
+++ bootstrap/teardown.sh
@@ -1,4 +1,6 @@
 # Cleanup script to delete the cluster created by the init.sh script
+# Try to get the project id from gcloud
+export PROJECT_ID=$(gcloud config get-value project)
 # exit if PROJECT_ID is not set
 if [[ -z "${PROJECT_ID}" ]]; then
   echo "The value of PROJECT_ID is not set. Be sure to run export PROJECT_ID=YOUR-PROJECT first"
